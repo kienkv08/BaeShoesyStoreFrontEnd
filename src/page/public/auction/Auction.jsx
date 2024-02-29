171a54496a20f53b432bb6f91183cca9cc43fb92
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2024-02-29 11:02:14 +0700

    update auction 1

--- src/page/public/auction/Auction.jsx
+++ src/page/public/auction/Auction.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Header from '../../../components/common/header/header'
 import Footer from '../../../components/common/footer/footer'
+import "./Auction.css";
 const Auction = () => {
   return (
     <Container fluid>
       <Header></Header>
       <Row style={{ border: "1px solid black", width: "90%", margin: "auto" }}>
-        <Col xs={2}>
-            div
-        </Col>
+        <Col xs={2}>USER X 4</Col>
         <Col xs={8}>
-          <h1>YE - P909 trash color</h1>
+          <h1>YE - P909 Premium</h1>
           <img
             style={{ height: "300px", margin: "auto" }}
             src="https://sneakerdaily.vn/wp-content/uploads/2021/03/Giay-adidas-Yeezy-Foam-Runner-MXT-Moon-Grey-GV7904.png.webp"
@@ -24,9 +23,123 @@ const Auction = () => {
           <h2>Your Price:</h2>
           <button>Bid</button>
         </Col>
-        <Col xs={2}></Col>
-        <input type="text" className='border-2'  placeholder='Hello'/>
+        <Col xs={2}>USER X 4</Col>
+        <input type="number" className="border-2" placeholder="Enter bid" />
       </Row>
+      <div class="phone_body">
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Patric Venturini </div>
+            <div class="contact_msg">
+              Are you planning to play anything tonight?{" "}
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">9:20 PM</div>
+            <div class="chat_new grad_pb"> New </div>
+          </div>
+        </div>
+
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/11.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Adriano Canofre </div>
+            <div class="contact_msg">
+              Dude can you help me with my motorcycle?
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">8:12 PM</div>
+            <div class="chat_new grad_pb"> New </div>
+          </div>
+        </div>
+
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Patric Venturini </div>
+            <div class="contact_msg">
+              Are you planning to play anything tonight?{" "}
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">Yesterday</div>
+            <div class="chat_new grad_pb"> New </div>
+          </div>
+        </div>
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/11.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Adriano Canofre </div>
+            <div class="contact_msg">
+              Dude can you help me with my motorcycle?
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">Yesterday</div>
+          </div>
+        </div>
+
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Patric Venturini </div>
+            <div class="contact_msg">
+              Are you planning to play anything tonight?{" "}
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">Yesterday</div>
+          </div>
+        </div>
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/11.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Patric Venturini </div>
+            <div class="contact_msg">
+              Are you planning to play anything tonight?{" "}
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">Yesterday</div>
+          </div>
+        </div>
+
+        <div class="chat">
+          <img
+            class="chat_avatar"
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+          />
+          <div class="chat_info">
+            <div class="contact_name">Patric Venturini </div>
+            <div class="contact_msg">
+              Are you planning to play anything tonight?{" "}
+            </div>
+          </div>
+          <div class="chat_status">
+            <div class="chat_date">Yesterday</div>
+          </div>
+        </div>
+      </div>
       <Footer></Footer>
     </Container>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import DefaultLayout from "../components/layout/default/default.layout";
 import Register from "../page/authentication/register/register";
 import Demo from "../page/demo/demo";
 import Auction from "../page/public/auction/Auction";
-import OrderHistory from "../page/public/auction/OrderHistory"
+import OrderHistory from "../page/public/money/OrderHistory"
 
 const publicRoutes = [
   { path: "/register", component: Register, layout: DefaultLayout },
