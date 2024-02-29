d702e13f5fe76cf2d7a4e91acfed1bfcd46b01a3
doanhtu22 <doanhtudum22@gmail.com>
2024-02-29 10:37:38 +0700

    commit to TuDA

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -99,13 +99,13 @@ const Header = () => {
             </div>
 
             <Link
-              to="/"
+              to="/About"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
               About Us
             </Link>
             <Link
-              to="/"
+              to="/Contact"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
               Contact me
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ import Demo from "../page/demo/demo";
 import Home from "../page/public/home/home";
 import PostPage from "../page/public/post/PostPage";
 import ProductDetail from "../page/public/product_detail/ProductDetail";
+import About from "../page/public/aboutUS/AboutUsPage";
+import Contact from "../page/public/contactUs/ContactUsPage";
+import Policy from "../page/public/policyPage/PolicyPage";
 
 const publicRoutes = [
   { path: "/register", component: Register, layout: DefaultLayout },
@@ -13,5 +16,8 @@ const publicRoutes = [
   { path: "/", component: Home, layout: DefaultLayout },
   { path: "/post", component: PostPage, layout: DefaultLayout },
   { path: "/product-detail", component: ProductDetail, layout: DefaultLayout },
+  { path: "/About", component: About, layout: DefaultLayout },
+  { path: "/Contact", component: Contact, layout: DefaultLayout },
+  { path: "/Policy", component: Policy, layout: DefaultLayout },
 ];
 export { publicRoutes };
